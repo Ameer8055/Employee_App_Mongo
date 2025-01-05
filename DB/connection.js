f5c64352a51988e08a0c593645019a2d81684684
Ameer8055 <155080912+Ameer8055@users.noreply.github.com>
2025-01-05 11:19:16 +0530

    Update connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.mongodbUrl).then(()=> console.log('Connection is established'))
+mongoose.connect('mongodb+srv://Ameer123:Ameershah123@cluster0.ctwe0.mongodb.net/EmployeeDB?retryWrites=true&w=majority&appName=Cluster0').then(()=> console.log('Connection is established'))
 .catch(()=> console.log('Connection not estalished'));
 
 
